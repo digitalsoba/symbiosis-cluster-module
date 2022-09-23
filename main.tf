340e89a047fc90f0616b442d6dde487436c3fdd9
digitalsoba <11384115+digitalsoba@users.noreply.github.com>
2022-09-22 22:24:00 -0700

    update node pool resource

--- main.tf
+++ main.tf
@@ -3,8 +3,11 @@ resource "symbiosis_cluster" "cluster" {
   region = var.region
 }
 
-resource "symbiosis_node_pool" "example" {
+resource "symbiosis_node_pool" "node_pool" {
   cluster   = symbiosis_cluster.cluster.name
   node_type = var.node_type
   quantity  = var.quantity
+  depends_on = [
+    symbiosis_cluster.cluster
+  ]
 }
